236722d0ac139ffaeeab6b47c4ad48f9c6c78e32
trthanh09 <155623893+trthanh09@users.noreply.github.com>
2024-03-19 13:29:56 +0700

    ASM2_01.py

--- ASM2_01.py
+++ ASM2_01.py
@@ -5,69 +5,101 @@ class Stock:
 
     def add_price(self, open_price, close_price):
         self.prices.append((open_price, close_price))
-        self.open_price= open_price
-        self.close_price = close_price
-    def avg_diff(self):
-        return round(sum(close - open for open, close in self.prices) / len(self.prices), 3)
 
-    def high_low(self):
-        return max(close for open, close in self.prices), min(close for open, close in self.prices)
+    def average_price_difference(self):
+        total_diff = sum(close - open for open, close in self.prices)
+        return total_diff / len(self.prices)
 
-    def increasing_days(self):
-        return sum(1 for open, close in self.prices if close > open)
 
-stocks = {}
-
-def read_file():
-    fileName = input("Enter the file name: ")
-    with open(fileName,'r') as file:
-        line = file.readlines()
-        for line in file:
-            code, open_price, close_price = line.split(' ')
+def read_file(filename):
+    stocks = {}
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        num_stocks = int(lines[0])
+        for line in lines[1:]:
+            stock_info = line.split()
+            code = stock_info[0]
+            open_price = float(stock_info[1])
+            close_price = float(stock_info[2])
             if code not in stocks:
                 stocks[code] = Stock(code)
-            stocks[code].add_price(float(open_price), float(close_price))
-    for code in sorted(stocks.keys()):
-        print(f'{code}: {stocks[code].avg_diff()}')
+            stocks[code].add_price(open_price, close_price)
+    return stocks
 
-def search_stock():
-    code = input('Enter stock code: ')
+
+def print_sorted_stocks(stocks):
+    sorted_codes = sorted(stocks.keys())
+    for code in sorted_codes:
+        stock = stocks[code]
+        avg_diff = stock.average_price_difference()
+        print(f"{code}: {avg_diff:.3f}")
+
+
+def search_stock_by_code(stocks, code):
     if code in stocks:
-        high, low = stocks[code].high_low()
-        print(f'Highest closing price: {high}\nLowest closing price: {low}')
+        max_close = max(price[1] for price in stocks[code].prices)
+        min_close = min(price[1] for price in stocks[code].prices)
+        print(f"Max Close Price: {max_close:.3f}")
+        print(f"Min Close Price: {min_close:.3f}")
     else:
-        print('Stock code not found.')
+        print("Không tìm thấy mã chứng khoán.")
 
-def increasing_stocks():
-    for code in stocks:
-        if all(close > open for open, close in stocks[code].prices[:2]):
-            print(code)
 
-def max_increasing_days():
-    max_days = max(stocks[code].increasing_days() for code in stocks)
-    for code in stocks:
-        if stocks[code].increasing_days() == max_days:
-            print(code)
+def find_trending_stocks(stocks):
+    trending_stocks = []
+    for code, stock in stocks.items():
+        if len(stock.prices) >= 2:
+            if stock.prices[0][1] > stock.prices[0][0] and stock.prices[1][1] > stock.prices[1][0]:
+                trending_stocks.append(code)
+    return trending_stocks
+
+
+def find_longest_trending_stocks(stocks):
+    longest_trending = []
+    max_days = 0
+    for code, stock in stocks.items():
+        count = 0
+        for price in stock.prices:
+            if price[1] > price[0]:
+                count += 1
+        if count > max_days:
+            longest_trending = [code]
+            max_days = count
+        elif count == max_days:
+            longest_trending.append(code)
+    return longest_trending, max_days
+
 
 def main():
+    stocks = read_file("data.txt")
     while True:
-        print('1. Read file')
-        print('2. Search stock')
-        print('3. Increasing stocks')
-        print('4. Max increasing days')
-        print('5. Exit')
-        option = input('Choose an option: ')
-        if option == '1':
-            read_file()
-        elif option == '2':
-            search_stock()
-        elif option == '3':
-            increasing_stocks()
-        elif option == '4':
-            max_increasing_days()
-        elif option == '5':
-            print('Author: Nguyễn Trung Thành (HE191297)')
-            break
+        print("\nMENU:")
+        print("1. In thông tin tất cả mã chứng khoán và giá trị trung bình của hiệu (giá đóng cửa – giá mở cửa) trong 10 ngày")
+        print("2. Tìm kiếm theo mã chứng khoán")
+        print("3. Tìm kiếm những mã chứng khoán có xu hướng tăng")
+        print("4. Tìm mã có số ngày tăng lớn nhất")
+        print("5. Thoát chương trình")
+        choice = input("Nhập lựa chọn của bạn: ")
 
-if __name__ == '__main__':
+        if choice == '1':
+            print_sorted_stocks(stocks)
+        elif choice == '2':
+            code = input("Nhập mã chứng khoán cần tìm: ")
+            search_stock_by_code(stocks, code)
+        elif choice == '3':
+            trending_stocks = find_trending_stocks(stocks)
+            print("Các mã chứng khoán có xu hướng tăng đồng thời trong cả ngày 1 và ngày 2:")
+            print(trending_stocks)
+        elif choice == '4':
+            longest_trending, max_days = find_longest_trending_stocks(stocks)
+            print(f"Mã có số ngày tăng lớn nhất ({max_days} ngày):")
+            print(longest_trending)
+        elif choice == '5':
+            print("Tác giả: [Nguyễn Trung Thành] - [HE191297]")
+            break
+        else:
+            print("Lựa chọn không hợp lệ. Vui lòng nhập lại.")
+
+
+if __name__ == "__main__":
     main()
